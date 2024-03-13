e2f0af70930eef7361b2cd4284af5941583622e2
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-03-13 16:27:31 +0800

    Created a new api list called persons and update POST functionality

--- index.js
+++ index.js
@@ -55,6 +55,35 @@ let notes = [
   },
 ];
 
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+function generateId(db) {
+  const maxId = notes.length > 0 ? Math.max(...db.map((n) => n.id)) : 0;
+
+  return maxId + 1;
+}
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello Express!</h1>");
 });
@@ -78,15 +107,73 @@ app.delete("/api/notes/:id", (request, response) => {
 });
 
 app.post("/api/notes", (request, response) => {
-  const maxId = notes.length + 1;
+  const body = request.body;
 
-  const note = request.body;
-  note.id = maxId;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(notes),
+  };
 
   notes = notes.concat(note);
 
   response.status(201).json(note);
 });
+
+app.get("/api/persons", (request, response) => {
+  response.status(200).json(persons);
+}),
+  app.get("/api/persons/:id", (request, response) => {
+    response
+      .status(200)
+      .json(persons.find((person) => person.id === Number(request.params.id)));
+  });
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+
+  if (!name || !number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  if (persons.find((person) => person.name === name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name,
+    number,
+    id: generateId(persons),
+  };
+
+  persons = persons.concat(person);
+  response.status(201).json(person);
+});
+
+app.get("/info", (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`);
+}),
+  app.get("/api/info/", (request, response) => {
+    response.status(200).json(persons);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
